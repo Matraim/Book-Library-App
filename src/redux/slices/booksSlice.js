bd611a09727bbff4527110388da338fabb51e01a
Bismillah <cupertino@Matraims-MacBook-Pro.local>
2023-11-25 14:16:41 +0600

    book library app

---
+++ src/redux/slices/booksSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  books: [],
+};
+
+const booksSlice = createSlice({
+  name: "books",
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    deleteBook: (state, action) => {
+      return {
+        ...state,
+        books: state.books.filter((book) => book.id !== action.payload),
+      };
+    },
+    toggleFavorite: (state, action) => {
+      state.books.forEach((book) => {
+        if (book.id === action.payload) {
+          book.isFavorite = !book.isFavorite;
+        }
+      });
+    },
+  },
+});
+
+export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
+
+export const selectBooks = (state) => state.books.books;
+export const selectIsLoadingViaAPI = (state) => state.books.isLoadingViaAPI;
+
+export default booksSlice.reducer;
